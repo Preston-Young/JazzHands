ccac1d153face452d162b873245e8121a88282d2
Jordan Young <jordanby@uci.edu>
2021-11-03 14:21:46 -0700

    nearly finished album page

--- client/src/app/services/spotify.service.ts
+++ client/src/app/services/spotify.service.ts
@@ -84,12 +84,18 @@ export class SpotifyService {
 
   getAlbum(albumId:string):Promise<AlbumData> {
     //TODO: use the album endpoint to make a request to express.
-    return null;
+    return this.sendRequestToExpress(`/album/${albumId}`).then(data => {
+      return new AlbumData(data);
+    });
   }
 
   getTracksForAlbum(albumId:string):Promise<TrackData[]> {
     //TODO: use the tracks for album endpoint to make a request to express.
-    return null;
+    return this.sendRequestToExpress(`/album-tracks/${albumId}`).then(data => {
+      return data.items.map(track => {
+        return new TrackData(track);
+      })
+    });
   }
 
   getTrack(trackId:string):Promise<TrackData> {
