3bb12889c28aa4b32117be41abeb66d15cf42b76
Jordan Young <jordanby@uci.edu>
2021-12-02 14:42:22 -0800

    made component for media player page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { ArtistPageComponent } from './pages/artist-page/artist-page.component';
 import { TrackPageComponent } from './pages/track-page/track-page.component';
 import { AlbumPageComponent } from './pages/album-page/album-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PlayerPageComponent } from './pages/player-page/player-page.component';
 
 const routes: Routes = [
 	{ path: 'artist/:id', component: ArtistPageComponent},
 	{ path: 'track/:id', component: TrackPageComponent},
 	{ path: 'album/:id', component: AlbumPageComponent},
+	{ path: 'player', component: PlayerPageComponent},
 	{ path: '', component: HomePageComponent}
 ];
 
