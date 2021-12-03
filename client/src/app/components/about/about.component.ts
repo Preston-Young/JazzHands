f0479210b16b1d28daa4068c14ef721ed48b63b7
Preston <yaumanyoung@yahoo.com>
2021-12-02 17:36:23 -0800

    Open hand gesture opens Spotify profile

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PredictionEvent } from 'src/app/prediction-event';
 import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
@@ -17,13 +18,23 @@ export class AboutComponent implements OnInit {
   ngOnInit() {
   }
 
-  /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
-  In that function, update the name, profile_pic, and profile_link fields */
-  onAboutMe() {
+  openAboutMe() {
     this.service.aboutMe().then(response => {
       this.name = response.name;
       this.profile_pic = response.imageURL;
       this.profile_link = response.spotifyProfile;
     });
+  }
+
+  /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
+  In that function, update the name, profile_pic, and profile_link fields */
+  onAboutMe() {
+    this.openAboutMe();
+  }
+
+  onGestureChange(prediction_event: PredictionEvent) {
+    if (prediction_event.getPrediction() === "Open Hand") {
+      this.openAboutMe();
+    }
   }
 }
