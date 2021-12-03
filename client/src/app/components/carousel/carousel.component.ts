4dc59e1e51ef443f8050d89f2b2932eb669c5c82
Preston <yaumanyoung@yahoo.com>
2021-12-03 15:58:30 -0800

    Added functionality to manually move carousel with gestures

--- client/src/app/components/carousel/carousel.component.ts
+++ client/src/app/components/carousel/carousel.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { ResourceData } from '../../data/resource-data';
+import { HandtrackerComponent } from '../handtracker/handtracker.component';
+import { PredictionEvent } from 'src/app/prediction-event';
 
 @Component({
   selector: 'app-carousel',
@@ -10,10 +12,22 @@ export class CarouselComponent implements OnInit {
 	@Input() carouselId:string;
 	@Input() resources:ResourceData[];
   @Input() resourceType:string;
+  @ViewChild(HandtrackerComponent) child:HandtrackerComponent;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onGestureChange(prediction_event: PredictionEvent) {
+    if (prediction_event.getPrediction() === "Closed Hand") {
+      (<any>$('.carousel')).carousel('prev');
+    }
+    else if (prediction_event.getPrediction() === "Open Hand") {
+      (<any>$('.carousel')).carousel('next');
+    }
+    else if (prediction_event.getPrediction() === "Two Open Hands") {
+      this.child.stopDetection();
+    }
+  }
 }
